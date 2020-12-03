7fbc31c22a7ebe08904c4db7b2b5bab51cb4b674
Yuko Aoki <yuko.aoki@mavs.uta.edu>
2020-12-03 17:59:11 -0600

    Add c code

---
+++ 1205_Hello_World.c
@@ -0,0 +1,17 @@
+//For use in CSE 1205 Lab at UT Arlington
+//Written by Shawn Gieser
+
+//Include necessary dependecies
+#include <stdio.h>
+
+//program entry poing
+//argc - number of command line arguments
+//argv - array of strings containing command line arguments
+int main(int argc, char **argv)
+{
+	//output to terminal
+	printf("Hello 1205!\n");
+	
+	//end program
+	return 0;
+}
